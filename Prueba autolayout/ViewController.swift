fe52bff04e90b52881fdd05395c373004b3aec53
RoyMiranda <rodrigomiranda01426@gmail.com>
2024-10-15 15:04:27 -0600

    constraintsExcercise

--- Prueba autolayout/ViewController.swift
+++ Prueba autolayout/ViewController.swift
@@ -8,14 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let testTextField: UITextField = {
+    private let purpleTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "alskdjfhlkasd"
         textField.backgroundColor = .clear
         textField.textAlignment = .center
         textField.isEnabled = true
         textField.adjustsFontSizeToFitWidth = true
-        
+        textField.layer.borderColor = UIColor.purple.cgColor
+        textField.layer.borderWidth = 3
         textField.font = UIFont(name: "HelveticaNeue", size: 50)
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
@@ -30,10 +31,43 @@ class ViewController: UIViewController {
         label.text = "Texto de prueba"
     
         label.adjustsFontSizeToFitWidth = true
-        label.backgroundColor = .white
+        label.backgroundColor = .clear
+        label.layer.borderColor = UIColor.red.cgColor
+        label.layer.borderWidth = 3
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    private let blueLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .black
+        
+        label.font = UIFont(name: "HelveticaNeue", size: 50)
+        label.text = "Texto de prueba"
+    
+        label.adjustsFontSizeToFitWidth = true
+        label.backgroundColor = .clear
+        label.layer.borderColor = UIColor.cyan.cgColor
+        label.layer.borderWidth = 3
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let redLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .black
+        
+        label.font = UIFont(name: "HelveticaNeue", size: 50)
+        label.text = "Texto de prueba"
+    
+        label.adjustsFontSizeToFitWidth = true
+        label.backgroundColor = .clear
+        label.layer.borderColor = UIColor.red.cgColor
+        label.layer.borderWidth = 3
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,18 +77,12 @@ class ViewController: UIViewController {
 
     private func setupUI(){
         view.addSubview(testLabel)
-        view.addSubview(testTextField)
         
         NSLayoutConstraint.activate([
-            testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             testLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             testLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
             testLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.2),
-            
-            testTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            testTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 200),
-            testTextField.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
-            testTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.2),
         ])
     }
 }
